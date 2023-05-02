905e45b36e709c005ca1f33c7fa0be0d56235900
Davidedm99 <51082176+Davidedm99@users.noreply.github.com>
2023-05-02 19:50:17 +0200

    when too many info displayed the central box goes out of screen need to finiish the stat table

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@ let nameRef = document.getElementById("pkmn-name");
 let searchBtn = document.getElementById("search-btn");
 let pageBody = document.getElementById("result");
 
+let typePath = "./assets/images/types/";
+
+
 //fetch data from api
 let getPkmn = () =>{
     let pkmnName = nameRef.value;
@@ -28,6 +31,7 @@ let getPkmn = () =>{
                         typeString = typeString.concat(data.types[i].type.name, " ");
                     }
                 }
+                
 
                 //elaborate the typing
                 var abilityString = "";
@@ -53,17 +57,72 @@ let getPkmn = () =>{
                                             <h2 style="color:#d9bb14;">${data.name}</h2>
 
                                             <div class="typing">
-                                                <div>${typeString.split(" ", 2).join("</div><div>")}</div>
+                                                <img src=${typePath + data.types[0].type.name}.png> 
+                                                ${typeLenght===2 ? 
+                                                    `<img src=${typePath + data.types[1].type.name}.png>` :
+                                                ''}
                                             </div>
 
-                                            
-                                            <h2> Abilities: </h2>
+                                            <h3> Abilities: </h3>
                                             <div class="abilities">
                                                 <div>${abilityString.split(" ", 3).join("</div><div>")}</div>
                                             </div>
-
-                                            
                                         </div>
+
+                                        <div class="table">
+                                            <table class="statsTable">
+                                                <tbody>
+                                                    <tr>
+                                                        <th colspan="2">Base Stats</th>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="HPBar">
+                                                            <span style="color:green;">HP</span>
+                                                        </td>
+                                                        <td class="HP-RBar">
+                                                            <div class="statValue">
+                                                                <b class="HPValue">${data.stats[0].base_stat}</b>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="ATKBar">
+                                                            <span style="color:red;">ATTACK</span>
+                                                        </td>
+                                                        <td>
+                                                            <div class="statValue">
+                                                                <b class="ATKValue">${data.stats[1].base_stat}</b>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="DEFBar">
+                                                            <span style="color:blue;">DEFENSE</span>
+                                                        </td>
+                                                        <td></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="SPATKBar">
+                                                            <span style="color:orange;">SPATK</span>
+                                                        </td>
+                                                        <td></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="SPDEFBar">
+                                                            <span style="color:cyan;">SPDEF</span>
+                                                        </td>
+                                                        <td></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="SPEBar">
+                                                            <span style="color:purple;">SPEED</span>
+                                                        </td>
+                                                        <td></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+
                                     `;
             })
             //error catching
@@ -71,12 +130,13 @@ let getPkmn = () =>{
                 pageBody.innerHTML = `<h3 class="msg>Error Occured</h3>`;
                 console.log(Error);
             });
-        }
-        //pkmn doesn't exist
-        else{
-            pageBody.innerHTML = `<h3 class="errorMsg">Pokémon not Found!</h3>`
-        } 
     }
+    //pkmn doesn't exist
+    else{
+        pageBody.innerHTML = `<h3 class="errorMsg">Pokémon not Found!</h3>`
+    } 
+}
 
 searchBtn.addEventListener("click", getPkmn);
 window.addEventListener("load", getPkmn);
+
